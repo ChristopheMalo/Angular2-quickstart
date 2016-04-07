83d5ceea658a56f14c1bf7a8901a98da926a384a
ChristopheMalo <christof.malo@gmail.com>
2016-04-07 19:02:10 +0200

    feat: code the main file

---
+++ app/main.ts
@@ -0,0 +1,12 @@
+/**
+ * The Main file
+ * 
+ * Loads the root component (Import)
+ * Import Angular browser bootstrap
+ * 
+ * To launch the application
+ */
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
