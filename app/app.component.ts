1f1bd2e45d1cf81e0019a6af15a539060496d5ba
ChristopheMalo <christof.malo@gmail.com>
2016-04-07 18:23:07 +0200

    docs: comment the component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,17 @@
+/**
+ * The App Component
+ * 
+ * The root of the application
+ * Hosts the client user experience 
+ */
+// Import library
 import {Component} from 'angular2/core';
 
+// The decorator - What template use end hox to create the component
 @Component({
     selector: 'my-app',
     template: '<h1>My first Angular 2 application</h1>'
 })
 
+// The class controls the appearance and behavior of view through its template
 export class AppComponent { }
